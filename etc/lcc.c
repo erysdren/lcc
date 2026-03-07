5a6a6517d22088d9ad58591fdcbdebf6389d55be
erysdren <contact@erysdren.me>
2026-03-07 01:13:12 -0600

    lcc.c: use execv() OR execvp()

--- etc/lcc.c
+++ etc/lcc.c
@@ -217,6 +217,7 @@ char *basepath(char *name) {
 #define _P_WAIT 0
 extern int fork(void);
 extern int wait(int *);
+extern int execv(const char *, char *[]);
 extern int execvp(const char *, char *[]);
 
 static int _spawnvp(int mode, const char *cmdname, const char *const argv[]) {
@@ -227,7 +228,8 @@ static int _spawnvp(int mode, const char *cmdname, const char *const argv[]) {
 		fprintf(stderr, "%s: no more processes\n", progname);
 		return 100;
 	case 0:
-		execvp(cmdname, (char **)argv);
+		if (execv(cmdname, (char **)argv) == -1)
+			execvp(cmdname, (char **)argv);
 		fprintf(stderr, "%s: ", progname);
 		perror(cmdname);
 		fflush(stdout);
