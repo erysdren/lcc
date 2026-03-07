63569d22fd52e08e48d5c9acc09ef91291c34c5f
erysdren <contact@erysdren.me>
2026-03-07 00:54:24 -0600

    lcc.c: use execvp() instead of execv() to search shell paths

--- etc/lcc.c
+++ etc/lcc.c
@@ -217,7 +217,7 @@ char *basepath(char *name) {
 #define _P_WAIT 0
 extern int fork(void);
 extern int wait(int *);
-extern int execv(const char *, char *[]);
+extern int execvp(const char *, char *[]);
 
 static int _spawnvp(int mode, const char *cmdname, const char *const argv[]) {
 	int pid, n, status;
@@ -227,7 +227,7 @@ static int _spawnvp(int mode, const char *cmdname, const char *const argv[]) {
 		fprintf(stderr, "%s: no more processes\n", progname);
 		return 100;
 	case 0:
-		execv(cmdname, (char **)argv);
+		execvp(cmdname, (char **)argv);
 		fprintf(stderr, "%s: ", progname);
 		perror(cmdname);
 		fflush(stdout);
